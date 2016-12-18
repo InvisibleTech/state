c4814c96a6079c849a9056332696705c723f2a13
John Ferguson <aninvisiblefriend.tech@gmail.com>
2016-12-18 16:10:12 -0500

    Added some assertions to the benchmark test to guard the best results.

--- src/test/java/org/invisibletech/CompareMethodPerformanceTest.java
+++ src/test/java/org/invisibletech/CompareMethodPerformanceTest.java
@@ -1,5 +1,11 @@
 package org.invisibletech;
 
+import static org.hamcrest.core.StringEndsWith.endsWith;
+import static org.hamcrest.number.OrderingComparison.greaterThan;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -12,6 +18,7 @@ import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.results.RunResult;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -20,34 +27,58 @@ import org.openjdk.jmh.runner.options.TimeValue;
 
 public class CompareMethodPerformanceTest {
 
+    static class InterestingBits {
+
+        public final String benchmark;
+        public final double score;
+
+        public InterestingBits(final String benchmark, final double score) {
+            this.benchmark = benchmark;
+            this.score = score;
+        }
+
+    }
+
     @Test
-    public void test() throws RunnerException {
+    public void shouldShowClusteredBy8IsFastest() throws RunnerException {
 
         final Options opt = new OptionsBuilder()
-                // Specify which benchmarks to run.
-                // You can be more specific if you'd like to run only one
-                // benchmark per test.
                 .include(this.getClass().getName() + ".*")
-                // Set the following options as needed
                 .mode(Mode.AverageTime)
                 .mode(Mode.Throughput)
                 .mode(Mode.SingleShotTime)
                 .timeUnit(TimeUnit.MICROSECONDS)
                 .warmupTime(TimeValue.seconds(1))
                 .warmupIterations(3)
-                .measurementTime(TimeValue.seconds(5))
+                .measurementTime(TimeValue.seconds(1))
                 .measurementIterations(6)
                 .threads(Runtime.getRuntime().availableProcessors() / 2)
                 .forks(1)
                 .shouldFailOnError(true)
                 .shouldDoGC(true)
-                // .jvmArgs("-XX:+UnlockDiagnosticVMOptions",
-                // "-XX:+PrintInlining")
-                // .addProfiler(WinPerfAsmProfiler.class)
                 .build();
 
-        new Runner(opt).run();
+        final Collection<RunResult> allResults = new Runner(opt).run();
 
+        assertEquals(9, allResults.size());
+
+        final List<InterestingBits> orderedAvgTimeResults = allResults.stream()
+                .filter(r -> r.getParams().getMode() == Mode.AverageTime)
+                .map(r -> new InterestingBits(r.getParams().getBenchmark(),
+                        r.getAggregatedResult().getPrimaryResult().getScore()))
+                .sorted((l, r) -> new Double(l.score).compareTo(r.score))
+                .collect(Collectors.toList());
+
+        assertEquals(3, orderedAvgTimeResults.size());
+
+        assertThat(orderedAvgTimeResults.get(0).benchmark, endsWith("benchmark8Clustered"));
+        assertThat(orderedAvgTimeResults.get(1).benchmark, endsWith("benchmark6Clustered"));
+        assertThat(orderedAvgTimeResults.get(2).benchmark, endsWith("benchmarkNonClustered"));
+
+        final long speedUpFactor = (long) (orderedAvgTimeResults.get(2).score
+                / orderedAvgTimeResults.get(0).score);
+
+        assertThat(speedUpFactor, greaterThan(3L));
     }
 
     // The JMH samples are the best documentation for how to use it
